df24586aa8c3276bdf4f7ef196d3f355d962ba72
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2025-06-16 20:16:19 +0700

    canvas

---
+++ canvas/lib/bindings.cjs
@@ -0,0 +1,45 @@
+'use strict'
+
+var bindings;
+if (process.platform === "win32") bindings = require('../build/win32/canvas.node')
+else bindings = require('../build/linux/canvas.node')
+
+module.exports = bindings
+
+Object.defineProperty(bindings.Canvas.prototype, Symbol.toStringTag, {
+  value: 'HTMLCanvasElement',
+  configurable: true
+})
+
+Object.defineProperty(bindings.Image.prototype, Symbol.toStringTag, {
+  value: 'HTMLImageElement',
+  configurable: true
+})
+
+bindings.ImageData.prototype.toString = function () {
+	return '[object ImageData]'
+}
+
+Object.defineProperty(bindings.ImageData.prototype, Symbol.toStringTag, {
+  value: 'ImageData',
+  configurable: true
+})
+
+bindings.CanvasGradient.prototype.toString = function () {
+	return '[object CanvasGradient]'
+}
+
+Object.defineProperty(bindings.CanvasGradient.prototype, Symbol.toStringTag, {
+  value: 'CanvasGradient',
+  configurable: true
+})
+
+Object.defineProperty(bindings.CanvasPattern.prototype, Symbol.toStringTag, {
+  value: 'CanvasPattern',
+  configurable: true
+})
+
+Object.defineProperty(bindings.CanvasRenderingContext2d.prototype, Symbol.toStringTag, {
+  value: 'CanvasRenderingContext2d',
+  configurable: true
+})
---
+++ canvas/lib/context2d.cjs
@@ -0,0 +1,11 @@
+'use strict'
+
+/*!
+ * Canvas - Context2d
+ * Copyright (c) 2010 LearnBoost <tj@learnboost.com>
+ * MIT Licensed
+ */
+
+const bindings = require('./bindings.cjs')
+
+module.exports = bindings.CanvasRenderingContext2d
